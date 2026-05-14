4ce0325e3022e650d863a66233b3b8f59eeffc6a
Jonathan Jisung Kim <jkim372@scc2102-19.ucdavis.edu>
2026-05-14 16:38:17 -0700

    Experiment

---
+++ every_picture/script.js
@@ -0,0 +1,118 @@
+(function() {
+    'use strict';
+    console.log('reading js');
+
+    const area1 = document.querySelector('#area1');
+    const area2 = document.querySelector('#area2');
+    const area3 = document.querySelector('#area3');
+    const area4 = document.querySelector('#area4');
+    const area5 = document.querySelector('#area5');
+    const area6 = document.querySelector('#area6');
+    const area7 = document.querySelector('#area7');
+    const area8 = document.querySelector('#area8');
+    const area9 = document.querySelector('#area9');
+    const area10 = document.querySelector('#area10');
+    const area11 = document.querySelector('#area11')
+    const area13 = document.querySelector('#area13');
+    const area14 = document.querySelector('#area14');
+    const area15 = document.querySelector('#area15');
+    const area16 = document.querySelector('#area16');
+    const area17 = document.querySelector('#area17');
+    const area18 = document.querySelector('#area18');
+    const area19 = document.querySelector('#area19');
+    const area20 = document.querySelector('#area20');
+    const area21 = document.querySelector('#area21');
+    const area22 = document.querySelector('#area22');
+    const area23 = document.querySelector('#area23');
+    const area24 = document.querySelector('#area24');
+    const area25 = document.querySelector('#area25');
+    const area26 = document.querySelector('#area26');
+    const area27 = document.querySelector('#area27');
+    const area28 = document.querySelector('#area28');
+    const area29 = document.querySelector('#area29');
+
+    area1.addEventListener('mouseover', function(){
+        console.log('mouseover area1');
+    });
+    area2.addEventListener('mouseover', function(){
+        console.log('mouseover area2');
+    });
+    area3.addEventListener('mouseover', function(){
+        console.log('mouseover area3');
+    });
+    area4.addEventListener('mouseover', function(){
+        console.log('mouseover area4');
+    });
+    area5.addEventListener('mouseover', function(){
+        console.log('mouseover area5');
+    });
+    area6.addEventListener('mouseover', function(){
+        console.log('mouseover area6');
+    });
+    area7.addEventListener('mouseover', function(){
+        console.log('mouseover area7');
+    });
+    area8.addEventListener('mouseover', function(){
+        console.log('mouseover area8');
+    });
+    area9.addEventListener('mouseover', function(){
+        console.log('mouseover area9');
+    });
+    area10.addEventListener('mouseover', function(){
+        console.log('mouseover area10');
+    });
+    area11.addEventListener('mouseover', function(){
+        console.log('mouseover area11');
+    });
+    area13.addEventListener('mouseover', function(){
+        console.log('mouseover area13');
+    });
+    area14.addEventListener('mouseover', function(){
+        console.log('mouseover area14');
+    });
+    area15.addEventListener('mouseover', function(){
+        console.log('mouseover area15');
+    });
+    area16.addEventListener('mouseover', function(){
+        console.log('mouseover area16');
+    });
+    area17.addEventListener('mouseover', function(){
+        console.log('mouseover area17');
+    });
+    area18.addEventListener('mouseover', function(){
+        console.log('mouseover area18');
+    });
+    area19.addEventListener('mouseover', function(){
+        console.log('mouseover area19');
+    });
+    area20.addEventListener('mouseover', function(){
+        console.log('mouseover area20');
+    });
+    area21.addEventListener('mouseover', function(){
+        console.log('mouseover area21');
+    });
+    area22.addEventListener('mouseover', function(){
+        console.log('mouseover area22');
+    });
+    area23.addEventListener('mouseover', function(){
+        console.log('mouseover area23');
+    });
+    area24.addEventListener('mouseover', function(){
+        console.log('mouseover area24');
+    });
+    area25.addEventListener('mouseover', function(){
+        console.log('mouseover area25');
+    });
+    area26.addEventListener('mouseover', function(){
+        console.log('mouseover area26');
+    });
+    area27.addEventListener('mouseover', function(){
+        console.log('mouseover area27');
+    });
+    area28.addEventListener('mouseover', function(){
+        console.log('mouseover area28');
+    });
+    area29.addEventListener('mouseover', function(){
+        console.log('mouseover area29');
+    });
+})();
